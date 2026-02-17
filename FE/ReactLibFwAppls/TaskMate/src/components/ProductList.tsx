77366e051f2d27808bb99b9d4b02908305e92c4c
Anil Kumar K K <akumarkk@outlook.com>
2026-02-17 21:33:12 +0000

    Refactor ProductList to use dynamic URL for fetching products and add in_stock property to mock data

--- FE/ReactLibFwAppls/TaskMate/src/components/ProductList.tsx
+++ FE/ReactLibFwAppls/TaskMate/src/components/ProductList.tsx
@@ -4,16 +4,17 @@ import './productList.css'
 export default function ProductList() {
 
   const [products, setProducts] = React.useState<string[]>([]);
+  const [url, setUrl] = React.useState<string>("http://localhost:8300/products");
 
   useEffect(() => {
-    fetch("http://localhost:8300/products")
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         setProducts(data);
         console.log("ProductList rendered", products.length, "products")
       });
     // console.log("ProductList rendered", products.length, "products")
-  }, []);
+  }, [url]);
 
   useEffect(() => {
     if (products.length > 0) {
@@ -23,8 +24,10 @@ export default function ProductList() {
 
   return (
     <div>ProductList
+        <button type="button" onClick={() => setUrl("http://localhost:8300/products?in_stock=true")}>In Stock Only</button>
+        <button type="button" onClick={() => setUrl("http://localhost:8300/products")}>All</button>
 
-
+<p>{products.length} products loaded</p>
       <section>
         {
           products.map(p => {
