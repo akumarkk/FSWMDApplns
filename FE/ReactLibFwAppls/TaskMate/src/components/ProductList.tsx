fe8b7963bb5a4a683a7261137ccc0f0059eda029
Anil Kumar K K <akumarkk@outlook.com>
2026-02-17 20:27:34 +0000

    setProducts async, can't await on setProducts

--- FE/ReactLibFwAppls/TaskMate/src/components/ProductList.tsx
+++ FE/ReactLibFwAppls/TaskMate/src/components/ProductList.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function ProductList() {
 
-    const [products, setProducts] = React.useState<string[]>([])
+  const [products, setProducts] = React.useState<string[]>([]);
 
-    console.log("ProductList rendered", products.length, "products")
+  useEffect(() => {
     fetch("http://localhost:8300/products?stock=15")
-    .then(response => response.json())
-    .then(data => setProducts(data));
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data);
+        console.log("ProductList rendered", products.length, "products")
+      });
+    // console.log("ProductList rendered", products.length, "products")
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      console.log("**ProductList rendered", products.length, "products");
+    }
+  }, [products]);
 
   return (
     <div>ProductList</div>
