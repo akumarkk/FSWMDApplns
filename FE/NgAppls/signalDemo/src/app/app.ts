3daef60a5724ae614d4f1898610f2697664565d3
Anil Kumar K K <akumarkk@outlook.com>
2026-02-27 23:05:45 +0000

    signal demo

--- FE/NgAppls/signalDemo/src/app/app.ts
+++ FE/NgAppls/signalDemo/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('signalDemo');
+  private counter = 0;
+
+  private timerSubscription?: Subscription;
+
+  ngOnInit() {
+    // interval(1000) emits a value every 1000ms (1 second)
+    this.timerSubscription = interval(1000).subscribe((val) => {
+      this.counter = val;
+    });
+  }
+
+  ngOnDestroy() {
+    // Clean up the timer when the user leaves the page
+    this.timerSubscription?.unsubscribe();
+  }
 }
