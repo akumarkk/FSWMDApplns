fd13a4fba8e2c7415223a71d0bb80107654afa83
Anil Kumar K K <akumarkk@outlook.com>
2026-03-01 13:14:21 +0000

    appl - component events

--- FE/ReactLibFwAppls/demoAppl/demoApplReact/src/Components/Item.tsx
+++ FE/ReactLibFwAppls/demoAppl/demoApplReact/src/Components/Item.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import './Item.css'
 
-export function Item(props: { name: string, calorie: number }) {
+export function Item(props: {item: { name: string, calorie: number }}) {
     return (
         <div className='item'>
-            <h3>Name : {props.name}</h3>
-            <h3>Calorie : {props.calorie}</h3>
+            <h3>Name : {props.item.name}</h3>
+            <h3>Calorie : {props.item.calorie}</h3>
         </div>
     );
 
--- FE/ReactLibFwAppls/demoAppl/demoApplReact/src/Components/List.tsx
+++ FE/ReactLibFwAppls/demoAppl/demoApplReact/src/Components/List.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import {Item} from "./Item";
+
+import './List.css'
 
 const initList = [
     {name : "tomato", calorie: 20},
@@ -7,13 +10,27 @@ const initList = [
 ];
 
 export function List() {
+    const listArr = useState(initList);
+    const [list, setList] = listArr;
+
+    const removeHigherCalorieItem = () => {
+        const newList = list.filter(item => item.calorie <= 100);
+        setList(newList);
+    }
+
     return (
         <div>
             <header>
                 <h2>
                     Grocery List
                 </h2>
-                <Item></Item>
+                {
+                    list.map((item, index) => {
+                        return <Item item={item} key={`item-${index}`}></Item>
+                    })
+                }
+
+                <button onClick={removeHigherCalorieItem} className="remove-btn"> Remove Unhealthy</button>
             </header>
         </div>
     );
